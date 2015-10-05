8c848bdbf9afe1588b7d0a624fb1b3c974e4e38b
Rick Vazquez <vazquezr@fontbonne.edu>
2015-10-05 16:55:49 -0500

    Added login menu button

--- app/src/main/java/edu/fontbonne/mobileapplab/flit/MainActivity.java
+++ app/src/main/java/edu/fontbonne/mobileapplab/flit/MainActivity.java
@@ -48,7 +48,9 @@ public class MainActivity extends Activity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_login) {
+            Intent loginIntent = new Intent(getApplicationContext(), LoginActivity.class);
+            startActivity(loginIntent);
             return true;
         }
 
