d634afb2f5305cc7db1fcc66d27e960ffbd83738
Rick Vazquez <vazquezr@fontbonne.edu>
2015-10-19 17:40:28 -0500

    Updated budget activity and added spending activity

--- app/src/main/java/edu/fontbonne/mobileapplab/flit/LoginActivity.java
+++ app/src/main/java/edu/fontbonne/mobileapplab/flit/LoginActivity.java
@@ -1,10 +1,10 @@
 package edu.fontbonne.mobileapplab.flit;
 
 import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends Activity {
 
@@ -12,5 +12,17 @@ public class LoginActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        EditText email = (EditText) findViewById(R.id.login_email_box);
+        EditText password = (EditText) findViewById(R.id.login_password_box);
+
+        Button submit = (Button) findViewById(R.id.login_btn_submit);
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
